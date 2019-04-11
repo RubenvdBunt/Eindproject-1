64b05fd2b48ba2ed74b9d513d639b20691a8b390
Ruben van de Bunt <rubenvdbunt@outlook.com>
2019-04-11 12:55:23 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <section class="footer">
       <div class="footer">
           <div class="wthree-copyright">
-              <p>© Joey Akse 2019</p>
+              <p>© Ruben van de Bunt & Joey Akse 2019</p>
           </div>
       </div>
 </section>
