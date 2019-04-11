b6713422ff39db8078b64aec76d3d0cb8551d351
Joey <joeyakse@live.nl>
2019-04-11 11:36:53 +0200

    footer

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<section class="footer">
+      <div class="footer">
+          <div class="wthree-copyright">
+              <p>© Joey Akse 2019</p>
+          </div>
+      </div>
+</section>
+</section>
+<?php
+include_once "includes/script.php";
+?>
+</section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -47,19 +47,6 @@ if(isset($_SESSION["email"])){
                         </div>
                     </div>
                 </section>
-
-                <!-- footer -->
-                <section class="footer">
-                      <div class="footer">
-                          <div class="wthree-copyright">
-                              <p>© Joey Akse 2019</p>
-                          </div>
-                      </div>
-                </section>
-            </section>
-            <?php
-                include_once "includes/script.php";
-            ?>
-        </section>
-    </body>
-</html>
+<?php
+ include ("footer.php");
+ ?>
