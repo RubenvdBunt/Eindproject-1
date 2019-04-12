c0f3740ad2af618726a9861a5c1f863ade5ad305
Joey <joeyakse@live.nl>
2019-04-12 11:26:11 +0200

    merge conflicts

--- docenten.php
+++ docenten.php
@@ -1,7 +1,6 @@
 <?php
 include ("includes/header.php");
  ?>
-<<<<<<< HEAD
         <section class="wrapper">
             <div class="wthree-font-awesome">
                 <div class="grid_3 grid_4 w3_agileits_icons_page">
@@ -30,49 +29,6 @@ include ("includes/header.php");
                             }else{
                                 echo "0 result";
                             }
-=======
-<!DOCTYPE html>
-<html>
-    <body>
-        <section id="container">
-            <?php
-            include_once "includes/logo.php";
-            include_once "includes/navbar.php";
-            ?>
-            </header>
-            <?php
-            include_once "includes/sidebar.php";
-            ?>
-
-            <section id="main-content">
-                <section class="wrapper">
-                    <div class="wthree-font-awesome">
-                        <div class="grid_3 grid_4 w3_agileits_icons_page">
-                            <div class="icons">
-                                <h2 class="titel">Docenten</h2>
-                                <button>Klas Toevoegen</button>
-                                <table class="klassen">
-                                    <tr>
-                                        <td>Voornaam</td>
-                                        <td>Achternaam</td>
-                                        <td>Mentor van klas</td>
-                                        <td>Email</td>
-                                        <td>Actie</td>
-                                    </tr>
-                                    <?php
-
-                                    $sql = "SELECT DocentVoornaam, DocentAchternaam, MentorKlas, DocentEmail FROM docent";
-                                    $result = $conn-> query($sql);
-
-                                    if($result-> num_rows > 0){
-                                        While($row = $result-> fetch_assoc()){
-                                            echo "<tr><td>" . $row["DocentVoornaam"] . "</td><td>" . $row["DocentAchternaam"] . "</td><td>" . $row["MentorKlas"] . "</td><td>" . $row["DocentEmail"] . "</td><td> <input type='button' value='Bewerken'><input type='button' value='Verwijderen'> </td></tr>";
-                                        }
-                                        echo "</table>";
-                                    }else{
-                                        echo "0 result";
-                                    }
->>>>>>> fa3c034185c5734a7e87e6fe6514d2b0be49eafa
 
                                     ?>
                             </div>
