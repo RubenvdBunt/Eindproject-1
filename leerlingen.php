36654c8bc9cf5339686df0aeae318f1255cb59a6
Joey <joeyakse@live.nl>
2019-04-12 11:23:07 +0200

    header include

--- leerlingen.php
+++ leerlingen.php
@@ -1,17 +1,6 @@
 <?php
 include ("includes/header.php");
  ?>
-<section id="container">
-    <?php
-    include_once "includes/logo.php";
-    include_once "includes/navbar.php";
-    ?>
-    </header>
-    <?php
-    include_once "includes/sidebar.php";
-    ?>
-
-    <section id="main-content">
         <section class="wrapper">
             <div class="wthree-font-awesome">
                 <div class="grid_3 grid_4 w3_agileits_icons_page">
