c03789745540da192c7d7b927b4b515c1285edda
Ruben van de Bunt <rubenvdbunt@outlook.com>
2019-05-08 10:02:36 +0200

    register

--- includes/register_check.php
+++ includes/register_check.php
@@ -1,9 +1,22 @@
 <?php
-    if(isset($_POST['register'])){
-        require "includes/conn.php";
+if(isset($_POST['register'])){
+    require "conn.php";
 
-        $name = $_POST['gebruikersnaam'];
-        $email = $_POST['email'];
-        $password1 = $_POST['password1'];
-        $password2 = $_POST['password2'];
+    $name = $_POST['gebruikersnaam'];
+    $email = $_POST['email'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+
+    if (empty($name) || empty($email) || empty($password1) || empty($password2)){
+        header("Location: ../register.php?error=emptyfields&gebruikerid=".$name."&email=".$email);
+        exit();
     }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        header("Location: ../register.php?error=invalidmail&gebruikerid=".$name);
+        exit();
+    }
+    elseif (!preg_match("/[a-zA-Z0-9]*$/")){
+        header("Location: ../register.php");
+        exit();
+    }
+}
