83fbf496009620606a5a090846ec95e96ad32edf
Joey <joeyakse@live.nl>
2019-05-08 11:33:16 +0200

    header teruggezet

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
-include_once "../../includes/conn.php";
-include_once "../../includes/login_check.php";
-
+include_once "includes/conn.php";
+include_once "includes/login_check.php";
 if(isset($_SESSION["email"])){
 //    echo '<h3>Login succesvol' .$_SESSION["email"]. '</h3>';
 }else{
     header("Location: login.php");
     exit();
 }
-
 //    if(isset($_SESSION["DocentID"]) || isset($_SESSION["GebruikerID"]) ){
 //        echo "gelukt";
 //    }else{
@@ -20,24 +18,20 @@ if(isset($_SESSION["email"])){
 <html>
 <head>
     <title>Eindproject leerjaar 2</title>
-    <link rel="stylesheet" href="../../css/bootstrap.min.css" >
-    <link href="../../css/style.css" rel='stylesheet' type='text/css' />
-    <link href="../../css/style-responsive.css" rel="stylesheet"/>
-    <link href="../../css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="jquery-ui.css">
-    <link rel="stylesheet" href="bootstrap.min.css" />
- 		<script src="jquery.min.js"></script>
- 		<script src="jquery-ui.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <link href="css/style-responsive.css" rel="stylesheet"/>
+    <link href="css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
   <section id="container">
       <?php
-      include_once "../../includes/logo.php";
-      include_once "../../includes/navbar.php";
+      include_once "includes/logo.php";
+      include_once "includes/navbar.php";
       ?>
       </header>
       <?php
-      include_once "../../includes/sidebar.php";
+      include_once "includes/sidebar.php";
       ?>
 
       <section id="main-content">
