6fe01ba47670172263d730c9e11de4b92e84b844
Ruben van de Bunt <rubenvdbunt@outlook.com>
2019-06-03 11:50:47 +0200

    register weghalen

--- login.php
+++ login.php
@@ -25,9 +25,6 @@
                     <input type="submit" class="submit" value="Log in" name="login">
 <!--                    echo password_hash("WACHTWOORD", PASSWORD_DEFAULT);-->
                 </form>
-                <form action="register.php" method="post">
-                    <input type="submit" class="submit" value="Account aanmaken" name="login">
-                </form>
             </div>
         </div>
         <script src="js/bootstrap.js"></script>
