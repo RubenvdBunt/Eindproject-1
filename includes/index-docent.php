157b97facabe2b7d9c84a095f2863327902f08f7
Joey <joeyakse@live.nl>
2019-06-17 13:14:50 +0200

    merge conflict

--- includes/index-docent.php
+++ includes/index-docent.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 if($_SESSION['DocentSession'] > 0){
   $query_docent = "SELECT * FROM docent LEFT JOIN gebruiker ON gebruiker.GebruikerID = docent.GebruikerID WHERE gebruiker.GebruikerID = ".$_SESSION["GebruikerID"]."";
   $statement_docent = $connect->prepare($query_docent);
@@ -35,22 +34,3 @@ if($_SESSION['DocentSession'] > 0){
   $GebruikerVoornaam = $result_docent["0"]["DocentVoornaam"];
 }
 ?>
-=======
-if($_SESSION['DocentSession'] > 0) {
-
-    //Haalt eigen informatie van docent op.
-    $query_docent = "SELECT DocentVoornaam, DocentAchternaam, DocentEmail FROM docent WHERE DocentID = 2";
-    $statement_docent = $connect->prepare($query_docent);
-    $statement_docent->execute();
-    $result_docent = $statement_docent->fetchAll();
-
-    //Haalt alle studenten op.
-    $query_studenten = "SELECT StudentVoornaam, StudentTussenvoegsel, StudentAchternaam  FROM student";
-    $statement_studenten = $connect->prepare($query_studenten);
-    $statement_studenten->execute();
-    $result_studenten = $statement_studenten->fetchAll();
-
-    $GebruikerVoornaam = $result_docent["0"]["DocentVoornaam"];
-
-}
->>>>>>> 503d93d168fdddbf8dc1607f33b4d1f655f3631d
